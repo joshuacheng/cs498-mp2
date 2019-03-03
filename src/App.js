b04662e7c23ce6c3ee8b204ee0df5aa8fc5ce2f8
joshuacheng <joshuac6@illinois.edu>
2019-03-03 17:48:25 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import poke_logo from './assets/pokemon-logo.png';
+import Pokemon from './Pokemon';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.baseUrl = 'https://pokeapi.co/api/v2/';
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={poke_logo} className="App-logo" alt="logo" />
+          <Pokemon baseUrl={ this.baseUrl }/>
         </header>
       </div>
     );
