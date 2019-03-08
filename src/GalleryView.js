f3f5cd30367ad84dd3e553986749e75e294623ac
joshuacheng <joshuac6@illinois.edu>
2019-03-07 21:09:40 -0600

    Oops meant to add these last commit

---
+++ src/GalleryView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class GalleryView extends Component {
+  render() {
+    return (
+      <div>
+        <p>hello world</p>
+      </div>
+    )
+  }
+}
+
+export default GalleryView
